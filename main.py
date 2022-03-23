02f245d7bc0e26a24219bde25adcf7dd6380375f
Renato Sortino <rensortino@gmail.com>
2022-03-23 17:37:59 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,73 @@
+import bpy
+import random
+from utils import *
+
+Y_RANGE = 3
+Z_RANGE = 3.5
+
+C = bpy.context
+D = bpy.data
+    
+def main():
+    collection_name = 'SG_Objects'
+    bpy.ops.object.delete()
+    object_classes = ['cube', 'cylinder', 'cone', 'sphere']
+    n_objs = 4
+    objects = [random.choice(object_classes) for _ in range(n_objs)]
+    relationships = [
+        [0, 'left to', 1],
+        [2, 'left to', 3],
+#        [0, 'left to', 3],
+#        [2, 'left to', 1],
+        [2, 'below', 0],
+        [1, 'below', 3],
+#        [2, 'below', 3],
+#        [1, 'below', 0]
+    ]
+    scene_graph = {'objects': objects, 'relationships': relationships}
+    obj_collection = get_or_create_collection(collection_name)
+
+    select_collection(collection_name)
+        
+    for obj in scene_graph['objects']:
+        if len(D.collections['SG_Objects'].all_objects) == len(scene_graph['objects']):
+            break
+        # Object
+        # shape = random.choice(['cone', 'cube', 'cylinder', 'sphere'])
+        new_obj = add_object(obj)
+
+        color = random.choice(list(COLORS.keys()))
+        mat = get_color_material(color)
+        add_material(new_obj, mat)
+        
+    for rel in scene_graph['relationships']:
+        s,p,o = rel
+        
+        subj = list(D.collections['SG_Objects'].all_objects)[s]
+        obj = list(D.collections['SG_Objects'].all_objects)[o]
+
+        if p == 'left to':
+            x_loc = -4.5
+            y_loc = round(random.gauss(-Y_RANGE / 2, Y_RANGE / 4))
+            z_loc = subj.location.z
+            # z_loc = round(random.gauss(0, 0.5), 2)
+            subj.location = [x_loc, y_loc, z_loc]
+            
+            y_loc = round(random.gauss(y_loc + 3, Y_RANGE / 8), 2)
+            # z_loc = round(random.gauss(0, 0.5), 2)
+            z_loc = subj.location.z
+            obj.location = [x_loc, y_loc, z_loc]
+
+        elif p == 'below':
+            x_loc = -4.5
+            # y_loc = round(random.gauss(0, 0.5), 2)
+            y_loc = subj.location.y
+            z_loc = round(random.gauss(-Z_RANGE / 2, Z_RANGE / 4))
+            subj.location = [x_loc, y_loc, z_loc]
+            
+            # y_loc = round(random.gauss(0, 0.5), 2)
+            y_loc = subj.location.y
+            z_loc = round(random.gauss(z_loc + 3, Z_RANGE / 8), 2)
+            obj.location = [x_loc, y_loc, z_loc]  
+    
+    select_objects_in_collection(collection_name)
---
+++ utils.py
@@ -0,0 +1,96 @@
+import bpy
+import json
+
+with open('properties.json') as j:
+    props = json.load(j)
+    COLORS = props['colors']
+    BG_COLORS = props['bg-colors']
+
+C = bpy.context
+D = bpy.data
+
+SCALE = [1.0, 1.0, 1.0]
+
+
+def rgb_to_rgba(rgb):
+    rgba = [round(c / 255, 4) for c in rgb]
+    rgba.append(1.0)
+    return rgba
+
+def get_color_material(color):
+    # Get material
+    mat = D.materials.get(f"{color.capitalize()} Material")
+    if mat is None:
+        # create material
+        mat = D.materials.new(name=f"{color.capitalize()} Material")
+        rgba_color = rgb_to_rgba(COLORS[color])
+        mat.diffuse_color = rgba_color
+    return mat
+
+def get_shape_function(shape):
+    if shape == 'cone':
+        return bpy.ops.mesh.primitive_cone_add
+    elif shape == 'cube':
+        return bpy.ops.mesh.primitive_cube_add
+    elif shape == 'cylinder':
+        return bpy.ops.mesh.primitive_cylinder_add
+    elif shape == 'sphere':
+        return bpy.ops.mesh.primitive_uv_sphere_add
+    else:
+        raise NotImplementedError
+
+def set_camera():
+    camera = D.objects['Camera']
+    camera.location = [5, 0, 0]
+    D.scenes["Scene"].render.resolution_percentage=100
+    D.scenes["Scene"].render.resolution_x = 512
+    D.scenes["Scene"].render.resolution_y = 512
+    D.scenes['Scene'].render.image_settings.color_mode='RGB'
+
+def set_background():
+    background = D.objects['Background']
+    background.location = [-5, 0, 0]
+    # D.images['background.mp4'].filepath = '/blender/data/00001486.jpg'
+    D.objects['Background'].scale[0] = 10
+    D.objects['Background'].scale[1] = 10
+    bg_mat = D.materials.get(f"background")
+    rgba_color = rgb_to_rgba(BG_COLORS['dark'])
+    bg_mat.diffuse_color = rgba_color
+
+def select_objects_in_collection(name):
+    for obj in D.collections[name].all_objects:
+        obj.select_set(True)
+
+def select_collection(name):
+    C.view_layer.active_layer_collection =  \
+        C.view_layer.layer_collection.children[name]
+
+def add_material(obj, mat):
+    # Assign it to object
+    if obj.data.materials:
+        # assign to 1st material slot
+        obj.data.materials[0] = mat
+    else:
+        # no slots
+        obj.data.materials.append(mat)
+
+def add_object(shape):
+    add_shape_fn = get_shape_function(shape)
+    add_shape_fn()
+    new_shape = C.view_layer.objects.active
+    new_shape.scale = SCALE
+    return new_shape
+
+def get_or_create_collection(name):
+    if D.collections.get(name):
+        obj_collection = D.collections[name]
+    else:
+        obj_collection = D.collections.new(name)
+        C.scene.collection.children.link(obj_collection)
+    return obj_collection
+
+def add_light():
+    bpy.ops.object.light_add(type='SUN', location=[0,0,10], rotation=[1,0,0])
+    light = C.active_object.data
+    light.energy = 1.5
+    light.color = [1,1,1]
